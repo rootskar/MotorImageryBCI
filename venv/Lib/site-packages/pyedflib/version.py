6f612a4632c8e2888e68f44f5d1e392c8ddcc485
Karel Roots <karel.roots@nortal.com>
2020-05-07 19:25:33 +0300

    Added venv libraries

---
+++ venv/Lib/site-packages/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.17'
+version = '0.1.17'
+full_version = '0.1.17'
+git_revision = '1b5df599ca7072a2a71fe3a77a82a4dd661ff679'
+release = True
+
+if not release:
+    version = full_version
