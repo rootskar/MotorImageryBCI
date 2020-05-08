6ebd8b9cb0c1b663cd98620f27df9707ee869aaf
Karel Roots <karel.roots@nortal.com>
2020-05-08 13:06:57 +0300

    Fixed Linux install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-python3 -m venv env &&
-source env/bin/activate && pip3 install wheel && pip3 install -r requirements.txt && pip3 install pyedflib==0.1.17
+python3.7 -m venv env &&
+source env/bin/activate && python3.7 -m pip install wheel && python3.7 -m pip install -r requirements.txt && python3.7
+-m pip install pyedflib==0.1.17
